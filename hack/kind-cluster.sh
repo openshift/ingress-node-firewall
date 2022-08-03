69bc81d936988d10fa6a50aade51642378fda11b
Mohamed Mahmoud <mmahmoud@redhat.com>
2022-08-03 06:37:43 -0400

    fix KIND to generate IPv4 and IPv6 node IPs
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- hack/kind-cluster.sh
+++ hack/kind-cluster.sh
@@ -5,20 +5,21 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # desired cluster name; default is "kind"
 KIND_CLUSTER_NAME="${KIND_CLUSTER_NAME:-kind}"
-
+IP_FAMILY=${IP_FAMILY:-dual}
+NET_CIDR_IPV4=${NET_CIDR_IPV4:-10.244.0.0/16}
+SVC_CIDR_IPV4=${SVC_CIDR_IPV4:-10.96.0.0/16}
+NET_CIDR_IPV6=${NET_CIDR_IPV6:-fd00:10:244::/48}
+SVC_CIDR_IPV6=${SVC_CIDR_IPV6:-fd00:10:96::/112}
 cat <<EOF | kind create cluster --image kindest/node:v1.24.0 --config=- --kubeconfig=${DIR}/kubeconfig
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 networking:
-    ipFamily: "dual"
+    podSubnet: $NET_CIDR_IPV4,$NET_CIDR_IPV6
+    serviceSubnet: $SVC_CIDR_IPV4,$SVC_CIDR_IPV6
+    ipFamily: $IP_FAMILY
 nodes:
 - role: control-plane
   kubeadmConfigPatches:
-  - |
-    kind: InitConfiguration
-    nodeRegistration:
-        kubeletExtraArgs:
-        container-log-max-size: "100Mi"
   - |
     kind: ClusterConfiguration
     apiServer:
