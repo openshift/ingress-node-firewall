7530e304d41acb72492f15fe769000fccdfcaa83
Andreas Karis <ak.karis@gmail.com>
2022-08-06 23:27:13 +0200

    Change IngressNodeFirewallNodeState Spec and Status
    Add Status field to IngressNodeFirewallNodeState.
    Combine []interfaces and rules field into map InterfaceIngressRules.
    Reimplement the reconciler and make it post reconciliation status to
    the resources.
    
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- controllers/ingressnodefirewall_controller.go
+++ controllers/ingressnodefirewall_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 
@@ -29,7 +30,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/sets"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
@@ -56,20 +56,9 @@ type IngressNodeFirewallReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
 func (r *IngressNodeFirewallReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	// 1) Get all IngressNodeFirewall objects (cluster scoped) and build the nodeStateSpecs.
-	//    nodeStateSpec is a map[<nodeName>]<IngressNodeFirewallNodeStateSpec>.
-	ingressNodeFirewallList := &infv1alpha1.IngressNodeFirewallList{}
-	if err := r.List(ctx, ingressNodeFirewallList); err != nil {
-		r.Log.Error(err, "Failed to list IngressNodeFirewall objects")
-		return ctrl.Result{}, err
-	}
-	nodeStateDesiredSpecs, err := r.buildNodeStateSpecs(ctx, ingressNodeFirewallList)
-	if err != nil {
-		r.Log.Error(err, "Failed to build IngressNodeFirewallNodeState")
-		return ctrl.Result{}, err
-	}
-
-	// 2) Get all IngressNodeFirewallNodeState objects in namespace <namespace> (namespace scoped).
+	// 1) Get all IngressNodeFirewallNodeState objects in namespace <namespace> (namespace scoped).
+	r.Log.Info("Getting all IngressNodeFirewallNodeState objects in namespace",
+		"req.Name", req.Name, "r.Namespace", r.Namespace)
 	ingressNodeFirewallNodeStateList := infv1alpha1.IngressNodeFirewallNodeStateList{}
 	listOpts := []client.ListOption{
 		client.InNamespace(r.Namespace),
@@ -79,73 +68,126 @@ func (r *IngressNodeFirewallReconciler) Reconcile(ctx context.Context, req ctrl.
 		return ctrl.Result{}, err
 	}
 
+	// 2) Get all IngressNodeFirewall objects (cluster scoped) and build the nodeStateSpecs.
+	//    nodeStateSpec is a map[<nodeName>]<IngressNodeFirewallNodeStateSpec>.
+	r.Log.Info("Getting all IngressNodeFirewall objects", "req.Name", req.Name)
+	ingressNodeFirewallList := &infv1alpha1.IngressNodeFirewallList{}
+	if err := r.List(ctx, ingressNodeFirewallList); err != nil {
+		r.Log.Error(err, "Failed to list IngressNodeFirewall objects")
+		return ctrl.Result{}, err
+	}
+	r.Log.Info("Building the desired node state specs", "req.Name", req.Name)
+	desiredNodeStates, err := r.buildNodeStates(ctx, ingressNodeFirewallList)
+	if err != nil {
+		r.Log.Error(err, "Failed to build IngressNodeFirewallNodeState")
+		return ctrl.Result{}, err
+	}
+
 	// 3) Delete objects which should not be there, update existing objects and create missing ones.
 	var name string
 	for _, ingressNodeFirewallCurrentNodeState := range ingressNodeFirewallNodeStateList.Items {
 		// First, check if the desired specs contain a possibly new spec for the current node state object.
 		name = ingressNodeFirewallCurrentNodeState.Name
-		nodeStateDesiredSpec, ok := nodeStateDesiredSpecs[name]
+		desiredNodeState, ok := desiredNodeStates[name]
 		// If the current state object is not found in the list of desired states, then we must delete the current node
 		// state object.
 		if !ok {
+			r.Log.Info("Existing object not found in desired list, triggering deletion", "req.Name", req.Name)
 			if err := r.Delete(ctx, &ingressNodeFirewallCurrentNodeState); err != nil && !errors.IsNotFound(err) {
 				r.Log.Error(err, "Failed to delete node state",
+					"req.Name", req.Name,
 					"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallCurrentNodeState.Namespace,
 					"ingressNodeFirewallNodeState.Name", ingressNodeFirewallCurrentNodeState.Name)
 				return ctrl.Result{}, err
 			}
-			r.Log.Info("Deleted object",
-				"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallCurrentNodeState.Namespace,
-				"ingressNodeFirewallNodeState.Name", ingressNodeFirewallCurrentNodeState.Name)
 			continue
 		}
 
-		// If the node name was found, but the specs are equal, continue with the next current node state.
-		// Also, remove the object from the nodeStateDesiredSpecs so that we can later iterate over the items
-		// that must still be created.
-		if equality.Semantic.DeepEqual(ingressNodeFirewallCurrentNodeState.Spec, nodeStateDesiredSpec) {
-			r.Log.Info("No change detected, skipping",
+		// If the node name was found ...
+		// a) compare the specs. If the specs are different, update the current spec.
+		if !equality.Semantic.DeepEqual(ingressNodeFirewallCurrentNodeState.Spec, desiredNodeState.Spec) {
+			// Otherwise, if the Spec and/or Status does not match, update the current node state.
+			// Also, remove the object from the nodeStateDesiredSpecs so that we can later iterate over the items
+			// that must still be created.
+			r.Log.Info("Existing object found but it has a different Spec, triggering update",
+				"req.Name", req.Name,
+				"ingressNodeFirewallCurrentNodeState.Name", ingressNodeFirewallCurrentNodeState.Name)
+			// i) Update the Spec.
+			ingressNodeFirewallCurrentNodeState.Spec = desiredNodeState.Spec
+			err = r.Update(ctx, &ingressNodeFirewallCurrentNodeState)
+			if err != nil {
+				r.Log.Error(err, "Failed to update IngressNodeFirewallNodeState",
+					"req.Name", req.Name,
+					"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallCurrentNodeState.Namespace,
+					"ingressNodeFirewallNodeState.Name", ingressNodeFirewallCurrentNodeState.Name)
+				return ctrl.Result{}, err
+			}
+			// Report success via a log message.
+			r.Log.Info("Updated object Spec",
+				"req.Name", req.Name,
 				"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallCurrentNodeState.Namespace,
 				"ingressNodeFirewallNodeState.Name", ingressNodeFirewallCurrentNodeState.Name)
-			delete(nodeStateDesiredSpecs, name)
-			continue
 		}
-
-		// Otherwise, if the specs do not match, update the current node state.
-		// Also, remove the object from the nodeStateDesiredSpecs so that we can later iterate over the items
-		// that must still be created.
-		ingressNodeFirewallCurrentNodeState.Spec = nodeStateDesiredSpec
-		err = r.Update(ctx, &ingressNodeFirewallCurrentNodeState)
-		if err != nil {
-			r.Log.Error(err, "Failed to update IngressNodeFirewallNodeState",
+		// b) compare the status. If the status is different, update it.
+		if !equality.Semantic.DeepEqual(ingressNodeFirewallCurrentNodeState.Status, desiredNodeState.Status) {
+			// ii) Update the resource's status field. Unfortunately, we cannot do this at the same time as the
+			// Spec update, so this has to go into a second step.
+			r.Log.Info("Existing object found but it has a different Status, triggering Status update",
+				"req.Name", req.Name,
+				"ingressNodeFirewallCurrentNodeState.Name", ingressNodeFirewallCurrentNodeState.Name)
+			ingressNodeFirewallCurrentNodeState.Status = desiredNodeState.Status
+			err = r.Status().Update(ctx, &ingressNodeFirewallCurrentNodeState)
+			if err != nil {
+				r.Log.Error(err, "Failed to update IngressNodeFirewallNodeState status",
+					"req.Name", req.Name,
+					"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallCurrentNodeState.Namespace,
+					"ingressNodeFirewallNodeState.Name", ingressNodeFirewallCurrentNodeState.Name)
+				return ctrl.Result{}, err
+			}
+			// Report success via a log message.
+			r.Log.Info("Updated object Status",
+				"req.Name", req.Name,
 				"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallCurrentNodeState.Namespace,
 				"ingressNodeFirewallNodeState.Name", ingressNodeFirewallCurrentNodeState.Name)
-			return ctrl.Result{}, err
 		}
-		r.Log.Info("Updated object",
-			"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallCurrentNodeState.Namespace,
-			"ingressNodeFirewallNodeState.Name", ingressNodeFirewallCurrentNodeState.Name)
-		delete(nodeStateDesiredSpecs, name)
-		continue
+		// Also, remove the object from the nodeStateDesiredSpecs so that we can later iterate over the items
+		// that must still be created.
+		delete(desiredNodeStates, name)
 	}
 
 	// Create all node states which are still inside the remaining desired specs.
-	for nodeToCreate, nodeStateDesiredSpec := range nodeStateDesiredSpecs {
+	for nodeToCreate, desiredNodeState := range desiredNodeStates {
+		r.Log.Info("Object node found, triggering creation", "req.Name", req.Name, "nodeToCreate", nodeToCreate)
+		// a) Create the new resource - this will set the spec.
 		ingressNodeFirewallNodeState := infv1alpha1.IngressNodeFirewallNodeState{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      nodeToCreate,
 				Namespace: r.Namespace,
 			},
-			Spec: nodeStateDesiredSpec,
+			Spec: desiredNodeState.Spec,
 		}
 		err = r.Create(ctx, &ingressNodeFirewallNodeState)
 		if err != nil {
 			r.Log.Error(err, "Failed to create new IngressNodeFirewallNodeState",
+				"req.Name", req.Name,
 				"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallNodeState.Namespace,
 				"ingressNodeFirewallNodeState.Name", ingressNodeFirewallNodeState.Name)
 			return ctrl.Result{}, err
 		}
+		// b) Update the resource's status field. Unfortunately, we cannot do this at the same time as the
+		// actual creation, so this has to go into a second step.
+		ingressNodeFirewallNodeState.Status = desiredNodeState.Status
+		err = r.Status().Update(ctx, &ingressNodeFirewallNodeState)
+		if err != nil {
+			r.Log.Error(err, "Failed to update IngressNodeFirewallNodeState status after creation",
+				"req.Name", req.Name,
+				"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallNodeState.Namespace,
+				"ingressNodeFirewallNodeState.Name", ingressNodeFirewallNodeState.Name)
+			return ctrl.Result{}, err
+		}
+		// Report success via a log message.
 		r.Log.Info("Created object",
+			"req.Name", req.Name,
 			"ingressNodeFirewallNodeState.Namespace", ingressNodeFirewallNodeState.Namespace,
 			"ingressNodeFirewallNodeState.Name", ingressNodeFirewallNodeState.Name)
 	}
@@ -194,21 +236,23 @@ func (r *IngressNodeFirewallReconciler) SetupWithManager(mgr ctrl.Manager) error
 		Complete(r)
 }
 
-// buildNodeStateSpecs reads a list of *ingressnodefwv1alpha1.IngressNodeFirewallList and builds an appropriate mapping
-// of node => IngressNodeFirewallNodeStateSpec.
-// TODO: settle on a logic for the merges.
-func (r *IngressNodeFirewallReconciler) buildNodeStateSpecs(
-	ctx context.Context, ingressNodeFirewallList *infv1alpha1.IngressNodeFirewallList) (map[string]infv1alpha1.IngressNodeFirewallNodeStateSpec, error) {
+// buildNodeStates reads a list of *ingressnodefwv1alpha1.IngressNodeFirewallList and builds an appropriate mapping
+// of <nodeName> to IngressNodeFirewallNodeState.
+func (r *IngressNodeFirewallReconciler) buildNodeStates(
+	ctx context.Context, infList *infv1alpha1.IngressNodeFirewallList) (map[string]infv1alpha1.IngressNodeFirewallNodeState, error) {
 	var err error
 	nodeList := v1.NodeList{}
-	nodeIngressRuleSet := make(map[string][]infv1alpha1.IngressNodeFirewallRules)
-	nodeInterfaceSet := map[string]sets.String{}
+	nodeStates := make(map[string]infv1alpha1.IngressNodeFirewallNodeState)
 
-	// Build the rule sets and interface lists per node.
-	// For each IngressNodeFirewall object, retrieve all nodes that match the label selector.
-	// Now, for each matching node, combine all rules for this node (TODO: combine unique rules)
-	// and all interfaces.
-	for _, firewallObj := range ingressNodeFirewallList.Items {
+	// Build the NodeStates in a map [<nodeName>]IngressNodeFirewallNodeState.
+	// Iterate over all IngressNodeFirewall objects. Get all nodes that are matched by an IngressNodeFirewall object.
+	// IngressNodeFirewall objects apply their Ingress rules to a slice of Interfaces. We translate this for
+	// IngressNodeFirewallNodeState to a map [<interface name>][]IngressNodeFirewallNodeState.
+	// A tuple of {Nodes,Interface name} may be matched by multiple IngressNodeFirewall objects, so merge
+	// InterfaceIngressRules[<interface name>] if necessary.
+	// If any issue with processing is found along the way, set the node's SyncStatus to SyncError and skip the node
+	// in any further iteration.
+	for _, firewallObj := range infList.Items {
 		listOpts := []client.ListOption{
 			client.MatchingLabels(firewallObj.Spec.NodeSelector.MatchLabels),
 		}
@@ -217,28 +261,139 @@ func (r *IngressNodeFirewallReconciler) buildNodeStateSpecs(
 			return nil, err
 		}
 
+	withNextNode:
 		for _, node := range nodeList.Items {
-			if _, ok := nodeIngressRuleSet[node.Name]; !ok {
-				nodeIngressRuleSet[node.Name] = []infv1alpha1.IngressNodeFirewallRules{}
+			// Create the node state object if it does not exist yet. Otherwise, use the existing state.
+			// We use this additional variable because struct fields in a map cannot be manipulated directly in golang.
+			// At the end of this loop, we will write back the state to the map with nodeStates[node.Name] = state.
+			var state infv1alpha1.IngressNodeFirewallNodeState
+			if _, ok := nodeStates[node.Name]; ok {
+				state = nodeStates[node.Name]
+			} else {
+				state = infv1alpha1.IngressNodeFirewallNodeState{}
+				state.Spec.InterfaceIngressRules = make(map[string][]infv1alpha1.IngressNodeFirewallRules)
 			}
-			if _, ok := nodeInterfaceSet[node.Name]; !ok {
-				nodeInterfaceSet[node.Name] = sets.String{}
+
+			// Check the current synchronization status. If the status indicates a synchronization error then
+			// continue with the next node.
+			if state.Status.SyncStatus == infv1alpha1.SyncError {
+				continue withNextNode
 			}
-			nodeIngressRuleSet[node.Name] = append(nodeIngressRuleSet[node.Name], firewallObj.Spec.Ingress...)
-			if firewallObj.Spec.Interfaces != nil {
-				nodeInterfaceSet[node.Name].Insert(firewallObj.Spec.Interfaces...)
+			// Otherwise, the current status will be either empty or ok, hence simply
+			// set it to SyncOK.
+			state.Status.SyncStatus = infv1alpha1.SyncOK
+
+			// Now, iterate over all interfaces in the InrgessNodeFirewallSpec.
+			if len(firewallObj.Spec.Interfaces) == 0 {
+				state.Status = infv1alpha1.IngressNodeFirewallNodeStateStatus{
+					SyncStatus:       infv1alpha1.SyncError,
+					SyncErrorMessage: fmt.Sprintf("Invalid interface name - cannot provide an empty list"),
+				}
+				// Write back the state to the map and then continue with the next node.
+				nodeStates[node.Name] = state
+				continue withNextNode
 			}
+			for _, iface := range firewallObj.Spec.Interfaces {
+				// Verify if an invalid interface name was specified.
+				// On error, report the error in the status field and continue with the next node.
+				if !isLegalInterfaceName(iface) {
+					state.Status = infv1alpha1.IngressNodeFirewallNodeStateStatus{
+						SyncStatus:       infv1alpha1.SyncError,
+						SyncErrorMessage: fmt.Sprintf("Invalid interface name %s", iface),
+					}
+					// Write back the state to the map and then continue with the next node.
+					nodeStates[node.Name] = state
+					continue withNextNode
+				}
+				// Create the rules for the node spec if they do not yet exist for this interface.
+				if _, ok := state.Spec.InterfaceIngressRules[iface]; !ok {
+					state.Spec.InterfaceIngressRules[iface] = []infv1alpha1.IngressNodeFirewallRules{}
+				}
+				// Merge in rules.
+				state.Spec.InterfaceIngressRules[iface], err = mergeRuleSet(
+					state.Spec.InterfaceIngressRules[iface], firewallObj.Spec.Ingress)
+				// On error, report the error in the status field and continue with the next node.
+				if err != nil {
+					state.Status = infv1alpha1.IngressNodeFirewallNodeStateStatus{
+						SyncStatus:       infv1alpha1.SyncError,
+						SyncErrorMessage: fmt.Sprintf("Illegal ruleset merge operation, err: %q", err),
+					}
+					// Write back the state to the map and then continue with the next node.
+					nodeStates[node.Name] = state
+					continue withNextNode
+				}
+			}
+			// Write back the state to the map.
+			nodeStates[node.Name] = state
 		}
 	}
 
-	nodeStateSpecs := make(map[string]infv1alpha1.IngressNodeFirewallNodeStateSpec, len(nodeIngressRuleSet))
-	for node, ingressRule := range nodeIngressRuleSet {
-		interfaceList := nodeInterfaceSet[node].List()
-		nodeStateSpecs[node] = infv1alpha1.IngressNodeFirewallNodeStateSpec{
-			Ingress:    ingressRule,
-			Interfaces: interfaceList,
+	return nodeStates, nil
+}
+
+func isLegalInterfaceName(iface string) bool {
+	if iface == "" {
+		return false
+	}
+	return true
+}
+
+// mergeRuleSet merges 2 rulesets of type []infv1alpha1.IngressNodeFirewallRules.
+// Ruleset a and the returned ruleset will go into IngressNodeFirewallNodeState. Therefore, for ruleset a and for
+// the returned ruleset, SourceCIDRs must be of length 1.
+// Ruleset b comes from IngressNoeFirewall. Therefore, for ruleset b, SourceCIDRs can have any length >= 1.
+func mergeRuleSet(a, b []infv1alpha1.IngressNodeFirewallRules) ([]infv1alpha1.IngressNodeFirewallRules, error) {
+	var err error
+
+	// Go over each rule that shall be merged in.
+	for _, ruleB := range b {
+		// In the b slice, we can potentially have multiple sourceCIDRs per rule.
+		// In the a slice, we want to avoid this so that we won't run into any ambiguous situations with the
+		// uniqueness of Order.
+	withNextSourceCIDR:
+		for _, sourceCIDR := range ruleB.SourceCIDRs {
+			// Now, go over each existing rule in the already merged slice.
+			for i, ruleA := range a {
+				if len(ruleA.SourceCIDRs) != 1 {
+					return nil, fmt.Errorf(
+						"cannot merge into ruleset A with invalid SourceCIDRs: '%v'", ruleA.SourceCIDRs)
+				}
+				// If the CIDR already exists in A, then merge it in and continue with the next CIDR.
+				if ruleA.SourceCIDRs[0] == sourceCIDR {
+					a[i].FirewallProtocolRules, err = mergeFirewallProtocolRules(
+						ruleA.FirewallProtocolRules, ruleB.FirewallProtocolRules)
+					if err != nil {
+						return nil, err
+					}
+					continue withNextSourceCIDR
+				}
+			}
+			// If the CIDR was not found, append the rules to A.
+			a = append(a, infv1alpha1.IngressNodeFirewallRules{
+				SourceCIDRs:           []string{sourceCIDR},
+				FirewallProtocolRules: ruleB.FirewallProtocolRules,
+			})
 		}
 	}
+	return a, nil
+}
 
-	return nodeStateSpecs, nil
+// mergeFirewallProtocolRules merges slices b of type []infv1alpha1.IngressNodeFirewallProtocolRule int slice a
+// of type []infv1alpha1.IngressNodeFirewallProtocolRule. The function throws an error if duplicate orders are found.
+func mergeFirewallProtocolRules(a, b []infv1alpha1.IngressNodeFirewallProtocolRule) ([]infv1alpha1.IngressNodeFirewallProtocolRule, error) {
+	orderList := make(map[uint32]struct{})
+	for _, itemA := range a {
+		if _, ok := orderList[itemA.Order]; ok {
+			return nil, fmt.Errorf("duplicate order %d detected for rules in A", itemA.Order)
+		}
+		orderList[itemA.Order] = struct{}{}
+	}
+	for _, itemB := range b {
+		if _, ok := orderList[itemB.Order]; ok {
+			return nil, fmt.Errorf("duplicate order %d detected for rules in B", itemB.Order)
+		}
+		orderList[itemB.Order] = struct{}{}
+		a = append(a, itemB)
+	}
+	return a, nil
 }
