1e5fab26887915fa2c79e667bfc8544a76968cce
Mohamed Mahmoud <mmahmoud@redhat.com>
2022-08-25 06:15:20 -0400

    fix make lint ci job
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-. $(dirname "$0")/common.sh
+set -euo pipefail
 
-VERSION="v1.45.2"
-
-if [[ $(which podman) && $(podman --version) ]]; then
-  command=podman
-else
-  command=docker
+# pin golangci-lint version to 1.46.0
+VERSION=v1.46.0
+if [ "$#" -ne 1 ]; then
+    echo "Expected command line argument - container runtime (docker/podman) got $# arguments: $@"
+    exit 1
 fi
 
-${command} run --rm -v $(pwd):/app:z -w /app -e GO111MODULE=on golangci/golangci-lint:${VERSION} \
-	golangci-lint run --verbose --print-resources-usage --timeout=15m0s
+$1 run --security-opt label=disable --rm -v $(pwd):/app -w /app -e GO111MODULE=on golangci/golangci-lint:${VERSION} \
+	golangci-lint run --verbose --print-resources-usage \
+	--modules-download-mode=vendor --timeout=15m0s && \
+	echo "lint OK!"
