b33095096498e95ffc50ce9e9165a695ad951d5e
Mohamed Mahmoud <mmahmoud@redhat.com>
2022-10-28 17:23:43 -0400

    OCPBUGS-2934: unlink events unix socket during shutdown
    to be to reuse unix socket we have to unlink them
    when the events container is shuting down
    
    Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

--- cmd/syslog/syslog.go
+++ cmd/syslog/syslog.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"gopkg.in/mcuadros/go-syslog.v2"
 )
@@ -20,13 +23,40 @@ func main() {
 	server.SetFormat(syslog.RFC3164)
 	server.SetHandler(handler)
 
-	if err := server.ListenUnixgram(listenAddress); err != nil {
-		log.Fatal(err)
+	// check if syslog unix socket is already there then delete it.
+	if _, err := os.Stat(listenAddress); err == nil {
+		if err := os.Remove(listenAddress); err != nil {
+			log.Fatalf("error removing syslog socket %s: %s", listenAddress, err)
+		}
 	}
 
-	if err := server.Boot(); err != nil {
-		log.Fatal(err)
+	if err := server.ListenUnixgram(listenAddress); err != nil {
+		log.Fatalf("failed to listen to syslog unix socket %s: %s", listenAddress, err)
 	}
+
+	defer func() {
+		if err := os.Remove(listenAddress); err != nil {
+			log.Fatalf("error removing syslog socket %s: %s", listenAddress, err)
+		}
+	}()
+
+	if err := server.Boot(); err != nil {
+		log.Fatalf("failed to boot syslog server: %s", err)
+	}
+
+	// Unix sockets must be unlink()ed before being reused again.
+	// Handle common process-killing signals so we can gracefully shut down:
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
+	go func(c chan os.Signal) {
+		// Wait for a SIGINT or SIGKILL:
+		sig := <-c
+		log.Printf("Caught signal %s: shutting down.\n", sig)
+		// Stop listening (and unlink the socket if unix type):
+		if err := server.Kill(); err != nil {
+			log.Fatalf("failed to close server connections: %s", err)
+		}
+	}(sigc)
 
 	go func(channel syslog.LogPartsChannel) {
 		for logParts := range channel {
