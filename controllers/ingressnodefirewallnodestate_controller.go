67a6b86590544ae04b16492a1a9c430e8c54d253
msherif1234 <mmahmoud@redhat.com>
2023-05-18 11:02:15 -0400

    OCPBUGS-11888: handle daemonSet pods restart
    when delete daemonSet or daemon pods manually
    pods will get recreated but the interface will
    have older xdp attached to it
    
    Signed-off-by: msherif1234 <mmahmoud@redhat.com>
    (cherry picked from commit 76bd82fcc3dfa65985ddc66ecfa063ed57a2e395)

--- controllers/ingressnodefirewallnodestate_controller.go
+++ controllers/ingressnodefirewallnodestate_controller.go
@@ -99,7 +99,7 @@ func (r *IngressNodeFirewallNodeStateReconciler) Reconcile(ctx context.Context, 
 	}
 
 	r.Log.Info("Reconciling resource and programming bpf", "name", nodeState.Name, "namespace", nodeState.Namespace)
-	return r.reconcileResource(ctx, req, nodeState, false)
+	return r.reconcileResource(ctx, nodeState, false)
 }
 
 // SetupWithManager sets up the controller with the Manager.
@@ -115,7 +115,7 @@ var mock ebpfsyncer.EbpfSyncer = nil
 // reconcileResource reconciles the resource by getting the EbpfDaemon singleton's SyncInterfaceIngressRules method.
 // For mock tests, var mock can be overwritten.
 func (r *IngressNodeFirewallNodeStateReconciler) reconcileResource(
-	ctx context.Context, req ctrl.Request, instance *infv1alpha1.IngressNodeFirewallNodeState, isDelete bool) (ctrl.Result, error) {
+	ctx context.Context, instance *infv1alpha1.IngressNodeFirewallNodeState, isDelete bool) (ctrl.Result, error) {
 	if err := ebpfsyncer.GetEbpfSyncer(ctx, r.Log, r.Stats, mock).SyncInterfaceIngressRules(instance.Spec.InterfaceIngressRules, isDelete); err != nil {
 		return ctrl.Result{}, errors.Wrapf(err, "FailedToSyncIngressNodeFirewallResources")
 	}
