601efda3f1937f76fcfe5b486a070dd03cea5eec
msherif1234 <mmahmoud@redhat.com>
2023-05-27 09:24:13 -0400

    SDN-3979: adding disruption e2e coverage This is continuation of github.com/openshift/ingress-node-firewall/pull/221
    Signed-off-by: msherif1234 <mmahmoud@redhat.com>

--- test/e2e/pods/pods.go
+++ test/e2e/pods/pods.go
@@ -12,6 +12,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	podutil "k8s.io/kubernetes/pkg/api/v1/pod"
+	"k8s.io/utils/pointer"
 )
 
 func EnsureRunning(client *testclient.ClientSet, pod *corev1.Pod, namespace string,
@@ -23,7 +24,8 @@ func EnsureRunning(client *testclient.ClientSet, pod *corev1.Pod, namespace stri
 			return nil, err
 		}
 	}
-	err = wait.PollImmediate(retryInterval, timeout, func() (done bool, err error) {
+
+	err = wait.PollUntilContextTimeout(context.Background(), retryInterval, timeout, true, func(ctx context.Context) (done bool, err error) {
 		testPod, err = client.Pods(namespace).Get(context.Background(), created.Name, metav1.GetOptions{})
 		if err != nil {
 			return false, err
@@ -36,17 +38,22 @@ func EnsureRunning(client *testclient.ClientSet, pod *corev1.Pod, namespace stri
 	return testPod, err
 }
 
-func EnsureDeleted(client *testclient.ClientSet, pod *corev1.Pod, timeout time.Duration) error {
-	err := client.Pods(pod.Namespace).Delete(context.Background(), pod.Name, metav1.DeleteOptions{})
+func EnsureDeleted(client *testclient.ClientSet, pod *corev1.Pod, retryInterval, timeout time.Duration) error {
+	err := client.Pods(pod.Namespace).Delete(context.Background(), pod.Name, metav1.DeleteOptions{GracePeriodSeconds: pointer.Int64(0)})
 	if err != nil {
-		if errors.IsNotFound(err) {
-			return nil
-		}
+		return err
 	}
+	err = wait.PollUntilContextTimeout(context.Background(), retryInterval, timeout, true, func(ctx context.Context) (done bool, err error) {
+		_, err = client.Pods(pod.Namespace).Get(context.Background(), pod.Name, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			return true, nil
+		}
+		return false, err
+	})
 	return err
 }
 
-func EnsureDeletedWithLabel(client *testclient.ClientSet, ns, label string, timeout time.Duration) error {
+func EnsureDeletedWithLabel(client *testclient.ClientSet, ns, label string, retryInterval, timeout time.Duration) error {
 	podList, err := client.Pods(ns).List(context.TODO(), metav1.ListOptions{
 		LabelSelector: label,
 	})
@@ -54,11 +61,25 @@ func EnsureDeletedWithLabel(client *testclient.ClientSet, ns, label string, time
 		return err
 	}
 	for _, pod := range podList.Items {
-		if err = EnsureDeleted(client, &pod, timeout); err != nil {
+		if err = EnsureDeleted(client, &pod, retryInterval, timeout); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func GetPodWithLabelRestartCount(client *testclient.ClientSet, namespace, label string, timeout time.Duration) (int, error) {
+	podList, err := client.Pods(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: label})
+	if err != nil {
+		return 0, err
+	}
+	var count int
+	for _, pod := range podList.Items {
+		for _, containerStatus := range pod.Status.ContainerStatuses {
+			count += int(containerStatus.RestartCount)
+		}
+	}
+	return count, nil
 }
 
 func GetIPV4(ips []corev1.PodIP) string {
